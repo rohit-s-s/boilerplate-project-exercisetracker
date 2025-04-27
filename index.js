e80f31839647e1aafce72cf7ee8348f8a1404c9c
rohith-s-s <rohithss2001@gmail.com>
2025-04-28 00:05:28 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+let userData = [];
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  const _id = Math.round(Math.random()*1000,0).toString()
+  userData.push({username,_id})
+  res.json({username,_id})
 });
 
 
 
-
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const [_id, description, duration, date] = [
+    req.params._id,
+    req.body.description,
+    req.body.duration,
+    new Date(req.body.date).toDateString(),
+  ];
+  userData.forEach((arr,i)=>{
+    var username = arr.username
+    if(arr._id === _id) {return res.json({
+      _id,
+      username,
+      date,
+      duration,
+      description,
+    })};
+    res.json({"error":"invalid input"})
+  })
+ 
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
