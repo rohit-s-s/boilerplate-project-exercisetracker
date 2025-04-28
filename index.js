dc834bcf5f45ec1af23909a8abb0f4e5ba46d4cb
rohith-s-s <rohithss2001@gmail.com>
2025-04-28 21:22:44 +0530

    log file added

--- index.js
+++ index.js
@@ -16,32 +16,56 @@ let userData = [];
 
 app.post("/api/users", (req, res) => {
   const username = req.body.username;
-  const _id = Math.round(Math.random()*1000,0).toString()
-  userData.push({username,_id})
-  res.json({username,_id})
+  const _id = Math.round(Math.random() * 1000, 0).toString();
+  userData.push({ username, _id });
+  res.json({ username, _id });
 });
-
-
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   const [_id, description, duration, date] = [
     req.params._id,
     req.body.description,
-    req.body.duration,
+    parseInt(req.body.duration),
     new Date(req.body.date).toDateString(),
   ];
-  userData.forEach((arr,i)=>{
-    var username = arr.username
-    if(arr._id === _id) {return res.json({
-      _id,
-      username,
-      date,
-      duration,
-      description,
-    })};
-    res.json({"error":"invalid input"})
-  })
- 
+  userData.forEach((arr, i) => {
+    var username = arr.username;
+    if (arr._id === _id) {
+      return res.json({
+        _id,
+        username,
+        date,
+        duration,
+        description,
+      });
+    }
+    res.json({ error: "invalid input" });
+  });
+});
+
+//get request to all user data
+
+app.get("/api/users", (req, res) => {
+  res.json(userData);
+});
+
+//get request to get log of userdata
+
+app.get("api/users/:id/logs", (req, res) => {
+  const id = req.params.id;
+  const user = userData.filter((arr) => arr.id === id);
+  res.json({
+    _id: user.id,
+    username: user.username,
+    count: 1,
+    log: [
+      {
+        description: user.description,
+        duration: user.duration,
+        date: user.date,
+      },
+    ],
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
